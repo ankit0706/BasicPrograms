bb46d2cc451d9bc31941e7107efbb1050174e609
ankit0706 <46675158+ankit0706@users.noreply.github.com>
2020-12-26 18:27:35 +0530

    Update RemoveDuplicatesOldWay.java

--- BasicPrograms/src/com/sample/basics/RemoveDuplicatesOldWay.java
+++ BasicPrograms/src/com/sample/basics/RemoveDuplicatesOldWay.java
@@ -1,9 +1,11 @@
 package com.sample.basics;
-
+/**
+old way is to either use a Set i.e. list to set and then back to list
+A Guava API is also there which can do the same
+Or else we simply create one more list from original list
+checking each time before adding, if the element exists already using contains
+method
+*/
 public class RemoveDuplicatesOldWay {
-	//old way is to either use a Set i.e. list to set and then back to list
-	//A Guava API is also there which can do the same
-	//Or else we simply create one more list from original list
-	//checking each time before adding, if the element exists already using contains
-	//method
+	//no code here
 }
